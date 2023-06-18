b32ff823e6e9e9cc71da7851a1aab7165ee635c9
ABDULSALAM Ahmad Ayoola <33053528+treehays@users.noreply.github.com>
2023-06-18 12:47:27 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RLN_Serilog.Models;
+using System.Diagnostics;
+
+namespace RLN_Serilog.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        _logger.LogInformation("Requested the Index Page");
+        int count;
+        try
+        {
+            for (count = 0; count <= 5; count++)
+            {
+                if (count == 3)
+                {
+                    throw new Exception("RandomException");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception Caught");
+        }
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    public void OnGet()
+    {
+        _logger.LogInformation("Requested the Index Page");
+        int count;
+        try
+        {
+            for (count = 0; count <= 5; count++)
+            {
+                if (count == 3)
+                {
+                    throw new Exception("RandomException");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Exception Caught");
+        }
+
+    }
+}
